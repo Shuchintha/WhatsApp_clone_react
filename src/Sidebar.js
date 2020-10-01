be14e186f51e2c6ea3b41473c66541bfed3adef5
Shuchintha Srinivasa <shuchinthasrinivasa@Shuchinthas-MacBook-Pro.local>
2020-09-30 21:57:10 -0400

    changes made to side bar icons

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,22 +1,34 @@
-import { Avatar } from '@material-ui/core'
+import { Avatar, IconButton } from '@material-ui/core'
 import React from 'react'
-import SearchIcon from '@material-ui/icons/Search';
-// import { Avatar } from '@material-ui/core'
+import DonutLargeIcon from '@material-ui/icons/DonutLarge';
+import ChatIcon from '@material-ui/icons/Chat';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
 import "./Sidebar.css"
+import { SearchOutlined } from '@material-ui/icons';
 
 function Sidebar() {
     return (
         <div className="sidebar">
-        <div className="sidebar_header">
-        
-        <Avatar />
-        </div>
-        <div className="sidebar_search">
-        <SearchIcon />
-        </div>
 
-        <div className="sidebar_chats"></div>
-            
+            <div className="sidebar_header">
+                <IconButton><Avatar /></IconButton>
+                <div className="sidebar_headerRight">
+                    <IconButton><DonutLargeIcon /></IconButton>
+                    <IconButton><ChatIcon /></IconButton>
+                    <IconButton><MoreVertIcon /></IconButton>
+                </div>
+            </div>
+
+            <div className="sidebar_search">
+                <div className="sidebar_searchContainer">
+                    <IconButton><SearchOutlined /></IconButton>
+                    <input placeholder="Search or start new chat" type="text"></input>
+                </div>
+            </div>
+
+            <div className="sidebar_chats">
+            </div>  
+
         </div>
     )
 }
